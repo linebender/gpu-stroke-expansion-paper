40bd2002d39f3701852e49ad1d98fe58623df854
Arman Uguray <arman.uguray@gmail.com>
2024-04-28 19:36:54 -0700

    fix tag_monoid_gpu_timings axis labels

--- input_processing_plot.py
+++ input_processing_plot.py
@@ -4,6 +4,7 @@
 
 import matplotlib.pyplot as plt
 import matplotlib
+import matplotlib.ticker as tkr
 import numpy as np
 
 xlabels = ['spiral', 'lorenz', 'spirograph', 'blender', 'roads', 'waves', 'mmark-35k', 'mmark-60k', 'longpathdash']
@@ -16,16 +17,33 @@ rtx4090_ip = np.array([13.95, 13.78, 13.94, 13.92, 13.96, 14.30, 15.91, 32.42, 3
 rtx4090_flatten = np.array([13.35, 18.38, 13.64, 20.89, 31.72, 30.97, 131.03, 196.31, 279.69])
 
 
-plt.figure(figsize=(5, 4), layout='constrained')
+fig = plt.figure(figsize=(5, 4), layout='constrained')
+ax = fig.add_subplot()
 #plt.plot(segments, mali_ip, label='Mali-G78 (tag monoid)')
 #plt.plot(segments, mali_flatten, label='Mali-G78 (flatten)')
-plt.plot(segments, m1_ip, label='M1 Max (tag monoid)', linewidth=2)
-plt.plot(segments, m1_flatten, label='M1 Max (flatten)', linewidth=2)
-plt.plot(segments, rtx4090_ip, label='RTX 4090 (tag monoid)', linewidth=2)
-plt.plot(segments, rtx4090_flatten, label='RTX 4090 (tag flatten)', linewidth=2)
+ax.plot(segments, m1_ip, label='M1 Max (tag monoid)', linewidth=2)
+ax.plot(segments, m1_flatten, label='M1 Max (flatten)', linewidth=2)
+ax.plot(segments, rtx4090_ip, label='RTX 4090 (tag monoid)', linewidth=2)
+ax.plot(segments, rtx4090_flatten, label='RTX 4090 (flatten)', linewidth=2)
 plt.xlabel('Input Segments')
-plt.ylabel('Time ($\\mu$s)')
 plt.legend()
+
+def msfmt(x, pos):
+    s = f'{x/1000:,.2f}'
+    return s
+
+xfmt = tkr.FuncFormatter(lambda x, p: format(int(x), ','))
+yfmt = tkr.FuncFormatter(lambda x, p: f'{x/1000:,.1f}')
+ax.xaxis.set_major_formatter(xfmt)
+
+log_scale = False
+if log_scale:
+    plt.yscale('log')
+    plt.yticks([1, 100, 500, 1000, 1500, 3000], ['1', '100', '500', '1000', '1500', '3000'])
+    plt.ylabel('Time ($\\mu$s)')
+else:
+    ax.yaxis.set_major_formatter(yfmt)
+    plt.ylabel('Time (ms)')
 
 plt.savefig('tag_monoid_gpu_timings.eps')
 plt.show()
