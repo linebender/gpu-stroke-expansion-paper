cffdf08996a499083c56bf2a5f064d1c2a13b82f
Arman Uguray <arman.uguray@gmail.com>
2024-06-15 15:36:00 -0700

    Update GPU timing plots with new strongly correct measurements

--- gpu_plots.py
+++ gpu_plots.py
@@ -17,34 +17,34 @@ save_to_file = len(sys.argv) > 1 and sys.argv[1] == 'save' or \
 
 if do_test_scenes:
 	arcs = pd.DataFrame({
-		"long dash (arcs)": [16.01, 3.24, 0.559, 0.08245],
-		"mmark-70k (arcs)": [9.93, 1.83, 1.67, 0.13434],
-		"mmark-120k (arcs)": [25.32, 2.97, 2.38, 0.19562]
+		"long dash (arcs)": [16.01, 3.24, 0.74552, 0.11001],
+		"mmark-70k (arcs)": [9.93, 1.83, 2.60, 0.18588],
+		"mmark-120k (arcs)": [25.32, 2.97, 4.14, 0.25569]
 		}, index=["Mali-G78", "M1 Max", "GTX 980Ti", "RTX 4090"]
 	)
 	lines = pd.DataFrame({
-		"long dash (lines)": [24.42, 3.38, 1.35, 0.31733],
-		"mmark-70k (lines)": [20.30, 3.42, 2.95, 0.22960],
-		"mmark-120k (lines)": [39.41, 5.63, 4.34, 0.38593],
+		"long dash (lines)": [24.42, 3.38, 1.32, 0.34711],
+		"mmark-70k (lines)": [20.30, 3.42, 4.37, 0.30955],
+		"mmark-120k (lines)": [39.41, 5.63, 8.26, 0.48937],
 		}, index=["Mali-G78", "M1 Max", "GTX 980Ti", "RTX 4090"]
 	)
 else:
 	arcs = pd.DataFrame({
-		"spirograph (arcs)":[565.77, 146.03, 37.37, 13.48],
-		"lorenz (arcs)":[412.13, 82.14, 50.61, 18.48],
-		"spiral (arcs)":[529.88, 118.93, 35.97, 13.23],
-		"blender (arcs)":[599.37, 133.49, 51.05, 20.73],
-		"waves (arcs)":[1480, 158.11, 85.06, 33.75],
-		"roads (arcs)":[1400, 277.05, 87.63, 31.39]
+		"spirograph (arcs)":[565.77, 146.03, 77.45, 23.01],
+		"lorenz (arcs)":[412.13, 82.14, 58.17, 21.90],
+		"spiral (arcs)":[529.88, 118.93, 75.24, 24.48],
+		"blender (arcs)":[599.37, 133.49, 78.01, 24.85],
+		"waves (arcs)":[1480, 158.11, 108.83, 35.08],
+		"roads (arcs)":[1400, 277.05, 229.85, 54.93]
 		}, index=["Mali-G78", "M1 Max", "GTX 980Ti", "RTX 4090"]
 	)
 	lines = pd.DataFrame({
-		"spirograph (lines)":[638.10, 183.05, 47.92, 18.35],
-		"lorenz (lines)":[682.04, 133.18, 93.75, 31.98],
-		"spiral (lines)":[1200, 257.67, 63.08, 25.71],
-		"blender (lines)":[761.69, 172.18, 68.76, 28.10],
-		"waves (lines)":[3460, 270.72, 214.03, 74.02],
-		"roads (lines)":[1460, 366.60, 98.41, 34.63]
+		"spirograph (lines)":[638.10, 183.05, 94.70, 28.35],
+		"lorenz (lines)":[682.04, 133.18, 95.34, 36.71],
+		"spiral (lines)":[1200, 257.67, 188.99, 76.03],
+		"blender (lines)":[761.69, 172.18, 102.05, 32.85],
+		"waves (lines)":[3460, 270.72, 238.87, 79.49],
+		"roads (lines)":[1460, 366.60, 282.61, 62.43]
 		}, index=["Mali-G78", "M1 Max", "GTX 980Ti", "RTX 4090"]
 	)
 
@@ -67,12 +67,12 @@ yfmt = tkr.FuncFormatter(numfmt)
 
 if do_test_scenes:
     ylabel = 'Time (ms)'
-    filename = "test_scenes_gpu_timings.eps"
+    filename = "test_scenes_gpu_timings_strong.eps"
     xinches = 5
 else:
     ylabel = 'Time (ms)'
     ax.yaxis.set_major_formatter(yfmt)
-    filename = "nehab_gpu_timings.eps"
+    filename = "nehab_gpu_timings_strong.eps"
     xinches = 5.3
 
 ax.set_ylabel(ylabel)
