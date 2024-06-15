463e1a2106b979e092c54bd14b5c449dd0af2301
Arman Uguray <arman.uguray@gmail.com>
2024-06-14 23:11:35 -0700

    Point bench tool at vello:paper-revisions

--- gpu_plots.py
+++ gpu_plots.py
@@ -17,34 +17,34 @@ save_to_file = len(sys.argv) > 1 and sys.argv[1] == 'save' or \
 
 if do_test_scenes:
 	arcs = pd.DataFrame({
-		"long dash (arcs)": [8.11, 2.90, 0.559, 0.08245],
-		"mmark-70k (arcs)": [7.50, 0.943, 1.67, 0.13434],
-		"mmark-120k (arcs)": [13.17, 1.54, 2.38, 0.19562]
+		"long dash (arcs)": [16.01, 3.24, 0.559, 0.08245],
+		"mmark-70k (arcs)": [9.93, 1.83, 1.67, 0.13434],
+		"mmark-120k (arcs)": [25.32, 2.97, 2.38, 0.19562]
 		}, index=["Mali-G78", "M1 Max", "GTX 980Ti", "RTX 4090"]
 	)
 	lines = pd.DataFrame({
-		"long dash (lines)": [13.06, 3.11, 1.35, 0.31733],
-		"mmark-70k (lines)": [12.45, 1.73, 2.95, 0.22960],
-		"mmark-120k (lines)": [22.52, 2.87, 4.34, 0.38593],
+		"long dash (lines)": [24.42, 3.38, 1.35, 0.31733],
+		"mmark-70k (lines)": [20.30, 3.42, 2.95, 0.22960],
+		"mmark-120k (lines)": [39.41, 5.63, 4.34, 0.38593],
 		}, index=["Mali-G78", "M1 Max", "GTX 980Ti", "RTX 4090"]
 	)
 else:
 	arcs = pd.DataFrame({
-		"spirograph (arcs)":[350.59, 88.01, 37.37, 13.48],
-		"lorenz (arcs)":[361.09, 65.84, 50.61, 18.48],
-		"spiral (arcs)":[324.73, 104.48, 35.97, 13.23],
-		"blender (arcs)":[384.37, 86.25, 51.05, 20.73],
-		"waves (arcs)":[1300, 135.61, 85.06, 33.75],
-		"roads (arcs)":[748.49, 102.37, 87.63, 31.39]
+		"spirograph (arcs)":[565.77, 146.03, 37.37, 13.48],
+		"lorenz (arcs)":[412.13, 82.14, 50.61, 18.48],
+		"spiral (arcs)":[529.88, 118.93, 35.97, 13.23],
+		"blender (arcs)":[599.37, 133.49, 51.05, 20.73],
+		"waves (arcs)":[1480, 158.11, 85.06, 33.75],
+		"roads (arcs)":[1400, 277.05, 87.63, 31.39]
 		}, index=["Mali-G78", "M1 Max", "GTX 980Ti", "RTX 4090"]
 	)
 	lines = pd.DataFrame({
-		"spirograph (lines)":[404.16, 99.03, 47.92, 18.35],
-		"lorenz (lines)":[616.51, 99.88, 93.75, 31.98],
-		"spiral (lines)":[456.60, 106.41, 63.08, 25.71],
-		"blender (lines)":[517.80, 129.34, 68.76, 28.10],
-		"waves (lines)":[3120, 208.40, 214.03, 74.02],
-		"roads (lines)":[947.03, 113.48, 98.41, 34.63]
+		"spirograph (lines)":[638.10, 183.05, 47.92, 18.35],
+		"lorenz (lines)":[682.04, 133.18, 93.75, 31.98],
+		"spiral (lines)":[1200, 257.67, 63.08, 25.71],
+		"blender (lines)":[761.69, 172.18, 68.76, 28.10],
+		"waves (lines)":[3460, 270.72, 214.03, 74.02],
+		"roads (lines)":[1460, 366.60, 98.41, 34.63]
 		}, index=["Mali-G78", "M1 Max", "GTX 980Ti", "RTX 4090"]
 	)
 
